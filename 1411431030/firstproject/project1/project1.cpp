997e41f2e8b1868bb2db242c380cc391344cd734
hihi114514 <bkong8107@gmail.com>
2025-09-24 12:25:41 +0800

    第一周

---
+++ 1411431030/firstproject/project1/project1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a = 10;
+    int b = 5;
+    int c = a + b;
+    printf("變數C=");
+    printf("%d",c);
+    return 0;
+}
