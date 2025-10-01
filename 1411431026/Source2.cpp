b054696f5f9b5a75ac0efcb8ba7fa4af0c682dc5
jannie0518 <jannie0518@gmail.com>
2025-10-01 12:09:53 +0800

    Add files via upload

---
+++ 1411431026/Source2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int mymain(void)
+{
+	int x;
+
+	printf("請輸入你的成績:\n");
+	scanf_s("%d", &x);
+
+	if (x >= 60) {
+		printf("恭喜通過!");
+	}
+	if (x < 60) {
+		printf("再加油!");
+	}
+	return 0;
+}
