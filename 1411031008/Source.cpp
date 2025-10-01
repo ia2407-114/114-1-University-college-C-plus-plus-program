9fb58d4d8c8c7a0e2ec94c576d30716bb20f2da4
s1411031008ctl <s1411031008@gmail.com>
2025-10-01 12:11:19 +0800

    Add files via upload

--- 1411031008/Source.cpp
+++ 1411031008/Source.cpp
@@ -5,23 +5,16 @@
 //fuction main begins program execution
 int main(void)
 {
-	int z, p, r, q, w, x, y;
+	printf("Enter one integer score\n");
 
-	printf("Enter p\n");
-	scanf_s("%d", &p);
-	printf("Enter r\n");
-	scanf_s("%d", &r);
-	printf("Enter q\n");
-	scanf_s("%d", &q);
-	printf("Enter w\n");
-	scanf_s("%d", &w);
-	printf("Enter x\n");
-	scanf_s("%d", &x);
-	printf("Enter y\n");
-	scanf_s("%d", &y);
+	int y;
+	scanf("%d", &y);
 
-	z = p * r % q + w / x - y;
-
-	printf("%d * %d % %d + %d / %d - %d are the %d\n", p, r, q, w, x, y, z);
-	return 1;
+	if (y >= 60) {
+		printf("恭喜通過");
+	}
+	if (y < 60) {
+		printf("再加油");
+	}
+	return 0;
 }
