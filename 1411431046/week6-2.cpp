3cf45544ad1af78d2ef959f15d13b2f7d9c42b4d
kevin68827 <kevin68827@gmail.com>
2025-10-22 12:02:30 +0800

    Add files via upload

---
+++ 1411431046/week6-2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double p, r, aims, deposit;
+    int year = 0;
+    double amount;
+
+    printf("請輸入本金: ");
+    scanf_s("%lf", &p);
+
+    printf("請輸入年利率: ");
+    scanf_s("%lf", &r);
+
+    printf("請輸入每年定期存入金額: ");
+    scanf_s("%lf", &deposit);
+
+    printf("請輸入目標金額: ");
+    scanf_s("%lf", &aims);
+
+    amount = p;
+
+    printf("\nYear\tAmount\n");
+
+    while (amount < aims) {
+        year++;
+        amount = amount * (1.0 + r) + deposit;  // 先利息後存入
+        printf("%4d\t%.2f\n", year, amount);
+    }
+
+    printf("\n需要 %d 年才能達到存款目標 %.2f\n", year, aims);
+
+    return 0;
+}
