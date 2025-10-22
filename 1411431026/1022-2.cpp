f3a92de594f21ecba866afc6e2c21f792e4ed909
jannie0518 <jannie0518@gmail.com>
2025-10-22 12:04:31 +0800

    Add files via upload

---
+++ 1411431026/1022-2.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(void) {
+
+	double principal ;
+	double rate;
+	double deposit, aims;
+	double amount;
+	int year = 0;
+	
+	printf("請輸入本金:");
+	scanf_s("%lf", &principal);
+
+	printf("請輸入年利率:");
+	scanf_s("%lf", &rate);
+
+	printf("請輸入每年底定期存入金額:");
+	scanf_s("%lf", &deposit);
+	
+	printf("請輸入存款目標(amis):");
+	scanf_s("%lf", &aims);
+
+	printf("%4s%21s\n", "Year", "Amount on deposit");
+
+	amount = principal;
+
+	while (amount < aims) {
+		year = year + 1;
+		amount = amount * (1.0 + rate);
+		amount += deposit;
+
+		printf("%4u%21.2f\n", year, amount);
+
+	}
+	printf("共需 %d 年才能達成目標\n", year);
+
+}
