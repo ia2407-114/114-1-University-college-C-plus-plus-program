59183bb5805e2acf083394cd3c7f6f6c06b20bf6
hongjun1231 <december951231@gmail.com>
2025-12-31 11:37:35 +0800

    Add files via upload

---
+++ 1411431038/線性搜尋.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 10
+
+int linearSearch( int array[], int key, int size);
+void copy1(char* s1, const char* s2)
+{
+    for (; ((*s1 = *s2) != '\0'); ++s1, ++s2);
+}
+void swap(int* ptr1, int* ptr2) {
+    int hold = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = hold;
+}
+void swap(float* ptr1, float* ptr2) {
+    float hold = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = hold;
+}
+
+void sort_array(char name[][20], int stu[][SIZE], float avg[], int n) {
+    int i, j, temp;
+    float temp1;
+    char temp_name[20];
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (avg[j] < avg[j + 1]) {
+
+                strcpy_s(temp_name, name[j]);
+                strcpy_s(name[j], name[j + 1]);
+                strcpy_s(name[j + 1], temp_name);
+
+                swap(&stu[0][j], &stu[0][j + 1]);
+                swap(&stu[1][j], &stu[1][j + 1]);
+                swap(&stu[2][j], &stu[2][j + 1]);
+                swap(&avg[j], &avg[j + 1]);
+            }
+        }
+    }
+}
+
+int linearSearch( int array[], int key, int size) {
+    for (size_t n = 0; n < size; ++n) {
+
+        if (array[n] == key) {
+            return n;                    
+        }
+    }
+    return -1;                       
+}
+
+int main() {
+    const char* stu_name[SIZE] = { "Danny","Mary","Jimmy","Peter","Sue","John","Hearts", "Diamond","Clubs","Spades" };
+    char stu1_name[10][20];
+    for (int i = 0; i < SIZE; i++)
+        copy1(stu1_name[i], stu_name[i]);
+
+    int stu[3][SIZE] = { { 11, 15, 13, 18, 12, 17, 14, 19, 16, 20 },
+                         { 85, 92, 78, 85, 90, 85, 76, 95, 88, 85 },
+                         { 70, 80, 75, 90, 85, 70, 82, 92, 88, 70 } };
+
+    float average_scores[SIZE];
+
+    int i, searchKey;
+
+    printf("排序前：\n");
+    printf("姓名\t學號\t計概成績\t數學成績\t平均成績\n");\
+        for (i = 0; i < SIZE; i++) {
+            average_scores[i] = (stu[1][i] + stu[2][i]) / 2.0;
+        }
+    for (i = 0; i < SIZE; i++) {
+        printf("%s\t%d\t    %d\t\t    %d\t\t    %f\t\n", stu1_name[i], stu[0][i], stu[1][i], stu[2][i], average_scores[i]);
+    }
+
+
+    sort_array(stu1_name, stu, average_scores, SIZE);
+
+    printf("\n排序後（依平均成績）：\n");
+    printf("姓名\t學號\t計概成績\t數學成績\t平均成績\n");\
+
+        for (i = 0; i < SIZE; i++) {
+            printf("%s\t%d\t    %d\t\t    %d\t\t    %f\t\n", stu1_name[i], stu[0][i], stu[1][i], stu[2][i], average_scores[i]);
+        }
+    printf("Enter integer search key: ");
+    scanf_s("%d", &searchKey);
+    int index = linearSearch(stu[0], searchKey, SIZE);
+
+    if (index != -1) {
+        printf("學號：%d,數概成績：%d,平均成績:%d",stu[0][index], stu[1][index], stu[2][index]);
+    }
+    else {
+        puts("Value not found");
+    }
+    return 0;
+}
