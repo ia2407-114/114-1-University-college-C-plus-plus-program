709311ee6e863454217a95230de1b333a1fa4b8e
CentraiLeaf <russenjura6@gmail.com>
2026-02-25 11:46:16 +0800

    2026022501

---
+++ 1411431042/20260225/20260225.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    FILE* cfPtr;
+
+    if ((cfPtr = fopen("students.txt", "w")) == NULL) {
+        puts("File could not be opened");
+    }
+    else {
+        puts("Enter name, student ID, math score, computer score");
+        puts("Press Ctrl+Z then Enter to finish input.");
+        printf("? ");
+
+        char name[30];
+        unsigned int id;
+        double math, computer, average;
+
+        
+        while (!feof(stdin)) {
+            if (scanf("%29s %u %lf %lf", name, &id, &math, &computer) != 4) {
+                break;   
+            }
+
+            average = (math + computer) / 2.0;
+
+            fprintf(cfPtr, "%s %u %.2f %.2f %.2f\n",
+                name, id, math, computer, average);
+
+            printf("? ");
+        }
+
+        fclose(cfPtr);
+
+        puts("所有資料已存入 students.txt，程式結束。");
+    }
+
+    return 0;
+}
