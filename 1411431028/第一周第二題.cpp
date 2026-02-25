0e957bebb50b5d14178f0a0a53ee9155592ee581
WEI130100 <willy52486@gmail.com>
2026-02-25 11:14:58 +0800

    Add files via upload

---
+++ 1411431028/第一周第二題.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+struct student {
+    char name[20];
+    int id, computer, math;
+    double avg;
+};
+
+
+int main(void)
+{
+    FILE* cfPtr; // accounts.dat file pointer
+
+    // fopen opens the file; exits if file cannot be opened
+    if ((cfPtr = fopen("accounts.dat", "wb")) == NULL) {
+        puts("File could not be opened.");
+    }
+    else {
+        // create clientData with default information      
+        struct student blankstudent = { "", 0, 0, 0, 0.0 };
+
+        // output 100 blank records to file                              
+        for (unsigned int i = 1; i <= 100; ++i) {
+            fwrite(&blankstudent, sizeof(struct student), 1, cfPtr);
+        }
+
+        fclose(cfPtr); // fclose closes the file
+    }
+}
