4a8f5a98754f6463bfe1662fd60e9c124742f53d
s1411031008ctl <s1411031008@gmail.com>
2026-02-25 11:56:29 +0800

    Add files via upload

---
+++ 1411031008/20260225/work2.cpp
@@ -0,0 +1,39 @@
+// Fig. 11.10: fig11_10.c
+// Creating a random-access file sequentially
+#include <stdio.h>
+struct student {
+    int id;
+    char name[20];
+    int computer, math;
+    double avg;
+};
+
+// clientData structure definition            
+/*struct clientData {
+    unsigned int acctNum; // account number
+    char lastName[15]; // account last name    
+    char firstName[10]; // account first name   
+    double balance; // account balance      
+};*/
+
+int main(void)
+{
+    FILE* cfPtr; // accounts.dat file pointer
+
+    // fopen opens the file; exits if file cannot be opened
+    if ((cfPtr = fopen("accounts.dat", "wb")) == NULL) {
+        puts("File could not be opened.");
+    }
+    else {
+        // create clientData with default information       
+        struct student blankStudent = { 0, "", 0 , 0 , 0.0 };
+
+        // output 100 blank records to file                              
+        for (unsigned int i = 1; i <= 100; ++i) {
+            fwrite(&blankStudent, sizeof(struct student), 1, cfPtr);
+        }
+
+        fclose(cfPtr); // fclose closes the file
+    }
+}
+
