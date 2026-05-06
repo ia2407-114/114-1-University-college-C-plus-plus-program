1da54b7aaa7d2bdc615614c6887ba8b3c2906825
WEI130100 <willy52486@gmail.com>
2026-05-06 11:08:48 +0800

    Add files via upload
    第11周第一題 前序運算子多載

---
+++ 1411431028/Matrix.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    float** element;
+
+    Matrix(int m, int n)
+    {
+        row = m;
+        column = n;
+
+        // 配置mxn的二維陣列element之動態記憶體
+        element = new float* [m];
+        int i, j;
+        for (i = 0; i < m; i++)
+            element[i] = new float[n];
+    }
+
+    // 定義矩陣的「-」(負號)前置運算子的多載
+    Matrix operator!()
+    {
+        Matrix temp(row, column);
+        int i, j;
+        for (i = 0; i < row; i++)
+            for (j = 0; j < column; j++)
+                temp.element[i][j] = sqrt(this->element[i][j]);
+        return temp;
+    }
+};
