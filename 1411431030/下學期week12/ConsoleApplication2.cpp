c16bced18bc0179bde129c0204c4cf20e27515c1
hihi114514 <bkong8107@gmail.com>
2026-05-13 11:25:33 +0800

    運算子多載2+父子類別繼承

---
+++ 1411431030/下學期week12/ConsoleApplication2.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class pet
+{
+private:
+    string name;
+    int year;
+
+public:
+    pet()
+    {
+        cout << "執行父類別pet的建構元函式\n";
+        cout << "寵物資訊:\n";
+        cout << "名稱:";
+        cin >> name;
+        cout << "年齡:";
+        cin >> year;
+    }
+
+    ~pet()
+    {
+        cout << "執行父類別pet的解構元函式.\n";
+    }
+
+    void display()
+    {
+        cout << "\n寵物名稱:" << name << endl;
+        cout << "年齡:" << year << endl;
+    }
+};
+
+class dog :public pet
+{
+private:
+    int born;
+    int runspeed;
+
+public:
+    // airliner類別建構元函式,設定大型客機的資料  
+    dog()
+    {
+        cout << "\n執行子類別dog的建構元函式\n";
+        cout << "產仔數:";
+        cin >> born;
+        cout << "奔跑速度:";
+        cin >> runspeed;
+    }
+
+    // airliner類別解構元函式
+    ~dog()
+    {
+        cout << "執行子類別dog的解構元函式.\n";
+    }
+
+    // 顯示大型客機的資料
+    void display()
+    {
+        cout << "產仔數:" << born << endl;
+        cout << "奔跑速度:" << runspeed << endl;
+    }
+};
+
+class bird :public pet
+{
+private:
+    int born;  // 乘客人數   
+    int flyspeed;
+
+public:
+    // airliner類別建構元函式,設定大型客機的資料  
+    bird()
+    {
+        cout << "\n執行子類別bird的建構元函式\n";
+        cout << "生蛋數:";
+        cin >> born;
+        cout << "飛行速度:";
+        cin >> flyspeed;
+    }
+
+    // airliner類別解構元函式
+    ~bird()
+    {
+        cout << "執行子類別bird的解構元函式.\n";
+    }
+
+    // 顯示大型客機的資料
+    void display()
+    {
+        cout << "生蛋數:" << born << endl;
+        cout << "飛行速度:" << flyspeed << endl;
+    }
+};
+
+int main()
+{
+    dog air1;
+	bird air2;
+    air1.pet::display();
+    air1.dog::display();
+	air2.pet::display();
+	air2.bird::display();
+
+    return 0;
+}
---
+++ 1411431030/下學期week12/第一題/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Time.h"
+using namespace std;
+
+int main()
+{
+    Time t1(23, 59, 58);
+    Time t2(8, 24, 36);
+
+    cout << "Initial t1: " << t1 << endl;
+    cout << "Initial t2: " << t2 << endl;
+
+    cout << "\nTesting prefix increment on t1:" << endl;
+    cout << "++t1 is " << ++t1 << endl;
+    cout << "++t1 is " << ++t1 << " (should wrap to 00:00:00)" << endl;
+
+    cout << "\nTesting postfix increment on t2:" << endl;
+    cout << "t2++ is " << t2++ << " (original value)" << endl;
+    cout << "Now t2 is " << t2 << " (after increment)" << endl;
+
+    Time t3(10, 30, 0);
+    cout << "\nt3 is " << t3 << endl;
+    cout << "t3 += 70 is " << (t3 += 70) << " (added 70 seconds)" << endl;
+
+    return 0;
+}
