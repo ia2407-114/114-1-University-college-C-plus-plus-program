f11fd527425421c5d482dff7a854dec203bd6a79
CentraiLeaf <russenjura6@gmail.com>
2026-06-03 12:03:14 +0800

    060301

---
+++ 1411431042/0603/flight_classes.h
@@ -0,0 +1,49 @@
+#ifndef FLIGHT_CLASSES_H
+#define FLIGHT_CLASSES_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class flight_object
+{
+public:
+    string name;
+    int id;
+    int pilot;
+    float kerosene;
+    string manufacturer;
+
+    void create_flight_object(string obj);
+};
+
+class airliner : public virtual flight_object
+{
+protected:
+    int passenger;
+    int service_person;
+
+public:
+    void create_airliner();
+    void display();
+};
+
+class battleplane : public virtual flight_object
+{
+protected:
+    string weapon;
+
+public:
+    void create_battleplane();
+    void display();
+};
+
+class bombplane : public airliner, public battleplane
+{
+public:
+    void create_bombplane();
+    void display();
+};
+
+#endif
---
+++ 1411431042/0603/main.cpp
@@ -0,0 +1,12 @@
+#include "flight_classes.h"
+
+int main()
+{
+    bombplane bomb1;
+
+    bomb1.create_flight_object("ÅF¬µ¾÷");
+    bomb1.create_bombplane();
+    bomb1.display();
+
+    return 0;
+}
