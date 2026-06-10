b3d0dd0747564b8333a4b8e4451e31a398f04cf0
hihi114514 <bkong8107@gmail.com>
2026-06-10 11:52:54 +0800

    循序文字檔存取寫入

---
+++ 1411431030/下學期week16/第一、二題/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include"file.h"
+
+using namespace std;
+int main()
+{
+	write();
+	read();
+
+	return 0;
+}
---
+++ 1411431030/下學期week16/第一、二題/file.h
@@ -0,0 +1,12 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void write();
+void read();
+
+#endif FILE_H
